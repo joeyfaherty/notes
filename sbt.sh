dd8a07a8d6824e1862e5b2b2e514cf403e950022
Joey Faherty <joeyfaherty@live.ie>
2017-05-10 15:24:48 +0200

    Update sbt.sh

--- sbt.sh
+++ sbt.sh
@@ -3,3 +3,30 @@ put cursor in a test and press Alt + Shift + F10
 
 # package jar file
 $ sbt assembly
+
+# sbt goals
+sbt clean 	
+Deletes all generated files (in the target directory).
+
+sbt compile 	
+Compiles the main sources (in src/main/scala and src/main/java directories).
+
+sbt test 	
+Compiles and runs all tests.
+
+sbt console 	
+Starts the Scala interpreter with a classpath including the compiled sources and all dependencies. To return to sbt, type :quit, Ctrl+D (Unix), or Ctrl+Z (Windows).
+
+sbt run <argument>* 	
+Runs the main class for the project in the same virtual machine as sbt.
+
+sbt package 	
+Creates a jar file containing the files in src/main/resources and the classes compiled from src/main/scala and src/main/java.
+
+sbt help <command> 	
+Displays detailed help for the specified command. If no command is provided, displays brief descriptions of all commands.
+
+sbt reload 	
+Reloads the build definition (build.sbt, project/*.scala, project/*.sbt files). Needed if you change the build
+
+-------------------------------------------
